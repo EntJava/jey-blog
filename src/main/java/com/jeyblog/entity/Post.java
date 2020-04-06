7ae1ddd1325608c544ece4c139b17e4807123714
udarc <juwimana@madisoncollege.edu>
2020-04-06 09:38:30 -0500

    Updated Geet all Post to format dates

--- src/main/java/com/jeyblog/entity/Post.java
+++ src/main/java/com/jeyblog/entity/Post.java
@@ -1,5 +1,7 @@
 package com.jeyblog.entity;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlRootElement;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
@@ -9,14 +11,22 @@ import org.hibernate.annotations.GenericGenerator;
 import org.hibernate.annotations.UpdateTimestamp;
 
 import javax.persistence.*;
+import javax.xml.bind.annotation.XmlRootElement;
 import java.time.LocalDateTime;
 
+/**
+ * The type Post.
+ * How to format Date https://stackoverflow.com/questions/28802544/java-8-localdate-jackson-format
+ */
 @Table(name = "Post")
 @Entity(name = "Post")
 @Getter
 @Setter
 @ToString
 @EqualsAndHashCode
+@XmlRootElement
+
+@JacksonXmlRootElement(namespace = "urn:stackify:jacksonxml", localName = "postData")
 public class Post {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO,generator = "native")
@@ -28,16 +38,19 @@ public class Post {
     private String category;
     @Column(name = "publication_date")
     @EqualsAndHashCode.Exclude
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     private LocalDateTime pubDate;
     private String description;
 
     @EqualsAndHashCode.Exclude
     @CreationTimestamp
     @Column(name = "created_at")
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     private LocalDateTime createdAt;
 
     @EqualsAndHashCode.Exclude
     @UpdateTimestamp
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     @Column(name = "updated_at")
     private LocalDateTime updatedAt;
 }
--- src/main/java/com/jeyblog/services/BlogResource.java
+++ src/main/java/com/jeyblog/services/BlogResource.java
@@ -1,29 +1,70 @@
 package com.jeyblog.services;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.dataformat.xml.XmlMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import com.jeyblog.entity.Post;
 import com.jeyblog.perisistence.GenericDao;
 
 import javax.swing.*;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
 import javax.ws.rs.core.GenericEntity;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
-@Path("/blog")
+/**
+ * The type Blog resource.
+ */
+@Path("/")
 public class BlogResource {
     private GenericDao blogPostDao = new GenericDao<>(Post.class);
+    /**
+     * The Object mapper.
+     */
     ObjectMapper objectMapper;
+
+    /**
+     * Gets all posts json.
+     *
+     * @return the all posts json
+     * @throws JsonProcessingException the json processing exception
+     */
     @GET
-    @Path("/posts")
-    public Response getAllPosts() throws JsonProcessingException {
+    @Path("posts")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getAllPostsJSON() throws JsonProcessingException {
         List<Post> postList = blogPostDao.getAll();
         objectMapper =  new ObjectMapper();
-        objectMapper.enable(SerializationFeature.INDENT_OUTPUT);
-        String posts = objectMapper.writeValueAsString("All Posts");
+        objectMapper.registerModule(new JavaTimeModule())
+                .configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false)
+                .enable(SerializationFeature.INDENT_OUTPUT);
+        String posts = objectMapper.writeValueAsString(postList);
         return Response.status(200).entity(posts).build();
     }
+
+    /**
+     * Gets all posts xml.
+     *https://dzone.com/articles/solving-the-xml-problem-with-jackson
+     * @return the all posts xml
+     * @throws JsonProcessingException the json processing exception
+     */
+    @GET
+    @Path("posts")
+    @Produces(MediaType.APPLICATION_XML)
+    public Response getAllPostsXML() throws JsonProcessingException {
+        List<Post> postList = blogPostDao.getAll();
+        objectMapper = new XmlMapper();
+        objectMapper.enable(SerializationFeature.INDENT_OUTPUT);
+        String xml =  objectMapper.writeValueAsString(postList);
+        return Response.ok().entity(xml).build();
+    }
+
 }
