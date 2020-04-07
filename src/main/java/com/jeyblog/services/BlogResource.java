e10b0d06d1224a74e73a0244f725099cf2185719
Yia Xiong <yxiong15@madisoncollege.edu>
2020-04-06 22:39:51 -0500

    add getPostByID and updatePost response method to BlogResource

--- src/main/java/com/jeyblog/services/BlogResource.java
+++ src/main/java/com/jeyblog/services/BlogResource.java
@@ -7,10 +7,10 @@ import com.jeyblog.entity.Post;
 import com.jeyblog.perisistence.GenericDao;
 
 import javax.swing.*;
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
+import javax.ws.rs.*;
 import javax.ws.rs.core.GenericEntity;
 import javax.ws.rs.core.Response;
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Path("/blog")
@@ -25,5 +25,34 @@ public class BlogResource {
         objectMapper.enable(SerializationFeature.INDENT_OUTPUT);
         String posts = objectMapper.writeValueAsString("All Posts");
         return Response.status(200).entity(posts).build();
+    }
+
+    @GET
+    @Path("/{post}")
+    public Response getPostByID(@PathParam("post") int id) throws JsonProcessingException {
+        Post post = (Post) blogPostDao.getById(id);
+        objectMapper =  new ObjectMapper();
+        objectMapper.enable(SerializationFeature.INDENT_OUTPUT);
+        String output = objectMapper.writerWithDefaultPrettyPrinter().writeValueAsString(post);
+        return Response.status(200).entity(output).build();
+    }
+
+    @POST
+    @Path("/update")
+    public Response updatePost(
+            @FormParam("id") int id,
+            @FormParam("title") String title,
+            @FormParam("author") String author,
+            @FormParam("category") String category,
+            @FormParam("description") String description) throws JsonProcessingException {
+        Post post = (Post) blogPostDao.getById(id);
+        post.setTitle(title);
+        post.setAuthor(author);
+        post.setCategory(category);
+        post.setDescription(description);
+        blogPostDao.saveOrUpdate(post);
+        String output = "Post ID " + id + " has been successfully updated to: " + post.toString();
+
+        return Response.status(200).entity(output).build();
     }
 }
